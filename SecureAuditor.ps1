6dc0ac65e30720837550279031ada62e9c418929
Charley Wu <akunzai@gmail.com>
2023-01-04 19:39:09 +0800

    Progress bar can significantly impact cmdlet performance
    - https://github.com/PowerShell/PowerShell/issues/2138

--- SecureAuditor.ps1
+++ SecureAuditor.ps1
@@ -1,3 +1,9 @@
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+	# Progress bar can significantly impact cmdlet performance
+	# https://github.com/PowerShell/PowerShell/issues/2138
+	$ProgressPreference = 'SilentlyContinue'
+}
+
 Import-Module ([IO.Path]::Combine($PSScriptRoot, 'SecureAuditor.psm1')) -Force
 
 $i18n = Data {
