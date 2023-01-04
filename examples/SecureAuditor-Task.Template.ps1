f596c62861c1aaae1c266fa9eab196b6b60a4d0a
Charley Wu <akunzai@gmail.com>
2023-01-04 22:17:13 +0800

    Tuning Task template

--- examples/SecureAuditor-Task.Template.ps1
+++ examples/SecureAuditor-Task.Template.ps1
@@ -1,6 +1,17 @@
 $from = 'from@example.com'
 $to = 'to@example.com'
+$sendGridApiKey = $env:SENDGRID_API_KEY
+
+if ($sendGridApiKey) {
+	$mailerPath = [IO.Path]::Combine($PSScriptRoot, './SecureAuditor-SendGrid.ps1')
+	& $mailerPath -From $from -To $to -ApiKey $sendGridApiKey
+	return
+}
+
 $smtpServer = 'smtp.example.com'
+# $username = 'username'
+# $password = 'password' | ConvertTo-SecureString -AsPlainText -Force
+# $credential = New-Object Management.Automation.PSCredential ( $username, $password )
 
 $mailerPath = [IO.Path]::Combine($PSScriptRoot, './SecureAuditor-SendMail.ps1')
-& $mailerPath -From $from -To $to -SmtpServer $smtpServer
+& $mailerPath -From $from -To $to -SmtpServer $smtpServer # -Credential $credential
